7e24b954aa3ae5aecf229e080d6f848a822abb83
Jens Oliver Meiert <jens@meiert.com>
2026-05-28 10:27:59 +0200

    chore: remove unnecessary shadow and rounding
    Signed-off-by: Jens Oliver Meiert <jens@meiert.com>

--- scripts/generate-feeds.py
+++ scripts/generate-feeds.py
@@ -182,7 +182,7 @@ def generate_index(feeds_info, out_path, now_str, config_edit_url=None):
 \t\t</style>
 \t</head>
 \t<body class="bg-zinc-900 p-8 pb-3">
-\t\t<div class="max-w-3xl mx-auto p-4 rounded-md shadow-sm">
+\t\t<div class="max-w-3xl mx-auto p-4">
 \t\t\t<h1 class="mb-2 text-2xl">Anonymized Feeds</h1>
 \t\t\t<p class="mb-8 text-sm">Copies of {'<a href="' + config_edit_url + '">configured feeds</a>' if config_edit_url else 'configured feeds'} with item links rewritten to <a href="https://web.archive.org/" target="_blank">the Internet Archive</a>. Last updated: {now_str}.</p>
 \t\t\t<section class="border border-[#333] mb-8 px-6 py-2 rounded-md">
